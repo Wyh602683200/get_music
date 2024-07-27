1255bbbe522c63a6a2e537e1be79ed8d239b7a6f
Wyh602683200 <155012405+Wyh602683200@users.noreply.github.com>
2024-07-27 14:15:58 +0800

    Update get_music.py

--- get_music.py
+++ get_music.py
@@ -5,16 +5,12 @@ from bridge.context import ContextType
 from bridge.reply import Reply, ReplyType
 from common.log import logger
 
-BASE_URL_DM = "https://api.linhun.vip/api/qqyy?name=周杰伦&y=1&n=1&apiKey=72188823153-0203570171c-7f48b38111d" #https://api.qqsuu.cn/
-
+BASE_URL_DM = "https://www.hhlqilongzhu.cn/api/dg_qqmusic.php?gm=泡沫&n=1" 
 @plugins.register(name="get_music",
                   desc="搜歌曲",
                   version="1.0",
                   author="wyh",
                   desire_priority=100)
-
-
-
 
 class get_music(Plugin):
 
@@ -25,7 +21,7 @@ class get_music(Plugin):
         logger.info(f"[{__class__.__name__}] inited")
 
     def get_help_text(self, **kwargs):
-        help_text = f"发送【搜歌曲】 获取歌曲链接"
+        help_text = f"发送【搜歌曲】或 【搜音乐】 获取歌曲链接"
         return help_text
 
     def on_handle_context(self, e_context: EventContext):
@@ -34,7 +30,7 @@ class get_music(Plugin):
             return
         self.content = e_context["context"].content.strip()
         
-        if self.content.startswith("搜歌曲"):
+        if self.content.startswith("搜歌曲") or self.content.startswith("搜音乐"):
             logger.info(f"[{__class__.__name__}] 收到消息: {self.content}")
             reply = Reply()
             result = self.get_music()
@@ -52,37 +48,35 @@ class get_music(Plugin):
 
     def get_music(self):
         url = BASE_URL_DM
-        params = {"name":self.content.replace(" ", "")[3:]}            
+        params = {"gm":self.content.replace(" ", "")[3:]}            
         headers = {'Content-Type': "application/x-www-form-urlencoded"}
         try:
             # 主接口
-            response = requests.get(url=url, params=params, headers=headers,timeout=2)
+            response = requests.get(url=url, params=params, headers=headers,timeout=10)
             if response.status_code == 200:
-                json_data = response.json()
-                if json_data.get('code') == 200:
-                                        
-                    formatted_output = []
-
-                    basic_info = (
-                        f"☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆\n"
-                        f"⌛ 歌曲: {json_data['name']}\n",
-                        f"⌛ 作者: {json_data['author']}\n",
-                        f"⌛ 播放地址: {json_data['mp3']}\n"
-                    )
-                    formatted_output.append(basic_info)
-           
-
-                    return '\n'.join(['\n'.join(item) for item in formatted_output])
-                else:
-                    logger.error(f"主接口返回值异常:{json_data}")
-                    raise ValueError('not found')
+                # json_data = response.text                      
+                # formatted_output = []
+                # basic_info = (
+                #     f"☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆\n"
+                #     f"⌛ 歌曲: {json_data['name']}\n",
+                #     f"⌛ 作者: {json_data['singer']}\n",
+                #     f"⌛ 播放地址: {json_data['url']}\n"
+                # )
+                # basic_info = response.text      
+                # formatted_output.append(basic_info)
+                
+                # return '\n'.join(['\n'.join(item) for item in formatted_output])
+                return  response.text         
+                
+                
             else:
-                logger.error(f"主接口请求失败:{response.text}")
-                raise Exception('request failed')
+                logger.error(f"接口返回值异常:{json_data}")
+                raise ValueError('not found')
+
         except Exception as e:
             logger.error(f"接口异常：{e}")
                 
-        logger.error("所有接口都挂了,无法获取")
+        logger.error("接口异常")
         return None
 
 
